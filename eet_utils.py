5c4ca647750e07b87b9dfb595a22c3b4c87abd09
Nguyen Hoang Long <roman.long@email.cz>
2017-02-25 17:19:20 +0100

    we want to FIK

--- eet_utils.py
+++ eet_utils.py
@@ -16,7 +16,7 @@ def send_receipt(receipt, testing=True):
     else:
         eet_client = eet.EET(config.cert_path, config.cert_pass, provozovna=config.provozovna_number,
                 pokladna='pokladna001', testing=False, eet_url=config.eet_url)
-    payment = eet_client.create_payment(receipt.number, amount)
+    payment = eet_client.create_payment(receipt.number, amount, test=False)
     result = eet_client.send_payment(payment)
     print(result)
     return result
